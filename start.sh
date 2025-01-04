52cae4442b0de5c1fab6f6be0f7d1d681958c536
Rohan Sada <rohansada@Mac.lan>
2025-01-04 16:53:36 -0500

    Bug fix in start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ docker run --rm -d --name mongodb --network fr_network -p 27017:27017 -v ./datab
 echo "MongoDB started!"
 
 echo "Starting FR Server"
-docker run --rm -d -it --network fr_network -p 3000:3000 -w /usr/src/app --name tf_dev tf_dev python3 ./main.py ./config.json
+docker run --rm -d -it --network fr_network -p 3000:3000 --name tf_dev tf_dev python3 ./main.py ./config.json
 
 echo "Running on all addresses (0.0.0.0)
 Running on http://127.0.0.1:3000
